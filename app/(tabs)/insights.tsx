d848471ed15770effbb0ee67c4b92e7f0b3efe55
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2026-09-02 22:16:25 +0300

    send update

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,16 @@
+import { styled } from "nativewind";
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Insights = () => {
+    return (
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text>I</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Insights
